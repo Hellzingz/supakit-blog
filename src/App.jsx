420c45b3bac1a9c065dd0b452af94dd93bec6481
Supakit Jaratworapat <supakit.jaratworapat@gmail.com>
2025-08-04 19:32:47 +0700

    Initialize React project with Vite and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <section>
+      <h1 className='text-3xl'>My First React App on Vite</h1>
+    </section>
+    </>
+  )
+}
+
+export default App
